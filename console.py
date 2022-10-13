95c19e2a3211565cf2753711d49db3d8afe6e165
ayamorcito <izquierdoz.g@gmail.com>
2022-10-13 11:19:39 -0700

    console

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Console entry point for HBnB
+    object and data manipulation
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ Command """
+    intro = "HBNB COMMAND"
+    prompt = '(hbnb)'
+    file = None
+
+    def do_quit(self, args):
+        """ test """
+        return True
+
+    def do_help(self, args):
+        """ test """
+        return True
+
+    def do_EOF(self, args):
+        """ EOF reached - exit """
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
