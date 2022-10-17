05bb30a7d222f34f01f7a142e9c81bb791107f47
Ivan Hansen <ihk225@gmail.com>
2022-10-17 01:42:20 -0300

    Rollback 2.0

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel():
             Constructor function for BaseModel class.
         """
         if not kwargs or len(kwargs) == 0:
-            self.id = uuid4()
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
